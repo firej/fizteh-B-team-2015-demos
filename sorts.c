cdca7208eaec1455df73bbf89d06cef26dd64142
Bogdanov Evgeniy <evbogdanov@yandex-team.ru>
2015-10-19 11:55:03 +0300

    merge sort code

--- sorts.c
+++ sorts.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define ARR_SIZE 10000000
 
 void bubble_sort(unsigned int size, char * arr);
 void qsort_sort(unsigned int size, char * arr);
 void heap_sort(unsigned int size, char * arr);
+void merge_sort(void* array, unsigned int size, unsigned int width, int (*compar)(const void *, const void *));
+
 int compar(const void * p1, const void * p2) {
-    return 0; //*((char*)p1) - *((char*)p2);
+    return *((char*)p1) - *((char*)p2);
 }
 
 void print (unsigned int size, char* arr) {
-    for (int i = 0; i < ARR_SIZE; i++){
+    for (int i = 0; i < size; i++){
        printf("%5d", arr[i]);
     }
     printf("\n");
@@ -23,20 +26,54 @@ int main(int argc, char *argv[]) {
 	int start_time = time(NULL);
     char *arr = (char*) malloc(ARR_SIZE * sizeof(char));
     for (unsigned long long i = 0; i < ARR_SIZE; i++) {
-        arr[i] = rand() % 10000;
+        arr[i] = rand() % 100;
     }
     // bubble_sort(ARR_SIZE, arr);
-    // qsort(arr, ARR_SIZE, sizeof(char), compar);
     // print(ARR_SIZE, arr);
 
-    heap_sort(ARR_SIZE, arr);
+    // heap_sort(ARR_SIZE, arr);
+    // merge_sort(arr, ARR_SIZE, sizeof(char), compar);
+    // qsort(arr, ARR_SIZE, sizeof(char), compar);
+    // mergesort(arr, ARR_SIZE, sizeof(char), compar);
 
 	// printf("time: %d\n", time(NULL) - start_time);
-    printf("\n");
+    // printf("\n");
     // print(ARR_SIZE, arr);
     free(arr);
     return 0;
 }
+
+void merge_sort(void* array, unsigned int size, unsigned int width, int (*compar)(const void *, const void *)) {
+    if (size <= 1)
+        return;
+    merge_sort(array, size/2, width, compar);
+    merge_sort(array + size/2, size - size/2, width, compar);
+
+    void* tmpbuf = malloc(size * width);
+
+    unsigned int i = 0, j = size/2;
+    for (int l = 0; l < size; l++) {
+        if (i < size/2 && j < size) {
+            if (compar(&array[i], &array[j]) < 0) {
+                memcpy(&tmpbuf[l], &array[i++], width);
+            }
+            else {
+                memcpy(&tmpbuf[l], &array[j++], width);
+            }
+        }
+        else if (i == size/2) {
+            memcpy(tmpbuf + l, array + j, (size - j)*width);
+            break;
+        }
+        else {
+            memcpy(tmpbuf + l, array + i, (size/2 - i)*width);
+            break;
+        }
+    }
+    memcpy(array, tmpbuf, size * width);
+    free(tmpbuf);
+}
+
 
 void heapify(char * arr, unsigned int i, unsigned int j);
 void heap_sort(unsigned int size, char * arr) {
