ea84bce664cc011b7d5941a0513abe3624c85e5c
Evgeniy <firej@ya.ru>
2016-04-18 11:07:53 +0300

    Update graham_scan.cpp

--- graham_scan.cpp
+++ graham_scan.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include "gl/glut.h"
+#include <windows.h>
+
+#pragma comment(lib, "glut32.lib")
 
 #define SIGN(x) (((x) >= 0) ? (1) : (-1))
 #define MSQR(x) ((x)*(x))
@@ -34,16 +38,23 @@ std::ostream& operator << (std::ostream& c, Point p) {
 	return c << "[" << p.x << "," << p.y << "]";
 }
 
+double vector_product(Point p1, Point p2, Point p3) {
+	std::cout << p1 << p2 << p3 << (p2.x - p1.x)*(p3.y - p2.y) - (p2.y - p1.y)*(p3.x - p2.x) << std::endl;
+	return (p2.x - p1.x)*(p3.y - p2.y) - (p2.y - p1.y)*(p3.x - p2.x);
+}
+
 class Comparer {
 public:
 	Point p;
-	Comparer(Point p0): p(p0){}
+	Comparer(Point p0) : p(p0){}
 	bool operator () (Point p1, Point p2) {
 		if (p1 == p2) {
 			return false;
 		}
-		std::cout << "Functor called  " << p1 << p2 << "  " << (p1^p) << (p1.abs()*p.abs()) << (p2^p) << (p2.abs()*p.abs()) << std::endl;
-		return ((p1^p)/(p1.abs()*p.abs()) - (p2^p)/(p2.abs()*p.abs())) > 0;
+		double ang1 = acos((p1.x - p.x) / p.abs(p1));
+		double ang2 = acos((p2.x - p.x) / p.abs(p2));
+		std::cout << "Functor called  " << p1 << p2 << "  " << ang1 << " " << ang2 << std::endl; 
+		return ang1 < ang2;
 	}
 };
 
@@ -64,27 +75,121 @@ Point get_left_bottom(std::vector<Point> points) {
 }
 
 
-int main() {
-	std::vector<Point> points;
-	std::vector<Point> scan;
+std::vector<Point> points;
+std::vector<Point> src_points;
+std::vector<Point> scan;
+Point p1, p2;
+
+void Display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLineWidth(2.0);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_LINES);
+	glVertex2f(0.0f, -1.0f);
+	glVertex2f(0.0f, 10.0f);
+	glEnd();
+	glBegin(GL_LINES);
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(10.0f, 0.0f);
+	glEnd();
+
+	glPointSize(10.0);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glBegin(GL_POINTS);
+	for each (auto point in src_points) {
+		//std::cout << "X:" << point.x << " Y:" << point.y << std::endl;
+		glVertex2f(point.x, point.y);
+	}
+	glEnd();
+
+
+
+	glPointSize(5.0);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glBegin(GL_LINE_STRIP);
+	for each (auto point in scan) {
+		//std::cout << "X:" << point.x << " Y:" << point.y << std::endl;
+		glVertex2f(point.x, point.y);
+	}
+	glVertex2f(scan[0].x, scan[0].y);
+	glEnd();
+
+	glColor3d(1.0, 0.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex2f(p1.x, p1.y);
+	glVertex2f(p2.x, p2.y);
+	glEnd();
+
+	glFlush();
+}
+
+void Initialize() {
+	glClearColor(0.8, 1.0, 0.6, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 10.0, -1.0, 10.0, -5.0, 5.0);\
+}
+
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(100, 200);
+	glutCreateWindow("Our first GLUT application!");
+	glutDisplayFunc(Display);
+	Initialize();
 
 	points.push_back(Point(5, 5));
 	points.push_back(Point(4, 4));
-	points.push_back(Point(4, 6));
+	points.push_back(Point(3, 1));
 	points.push_back(Point(0, 0));
+	points.push_back(Point(2, 1));
 	points.push_back(Point(0, 4));
 	points.push_back(Point(2, 4));
 
-	scan.push_back(get_left_bottom(points));
+	src_points = points;
+
+	int start_length = points.size();
+	Point p0 = get_left_bottom(points);
+	scan.push_back(p0);
+
+	for (std::vector<Point>::iterator i = points.begin(); i < points.end(); i++) {
+		if (*i == p0) {
+			points.erase(i);
+			break;
+		}
+	}
+
 	Comparer c(scan[0]);
-
-	std::cout << "P0:" << scan[0] << std::endl;
-
 	std::sort(points.begin(), points.end(), c);
+	scan.push_back(points[0]);
+	points.erase(points.begin());
+
+	for (;points.size();) {
+		while (vector_product(*(++scan.rbegin()), *(scan.rbegin()), points[0]) < 0) {
+			scan.pop_back();
+		}
+		scan.push_back(points[0]);
+		points.erase(points.begin());
+	}
 
 	for each (auto point in points) {
 		std::cout << "X:" << point.x << " Y:" << point.y << std::endl;
 	}
 
+	double max_distance = 0;
+	for (unsigned int i = 0; i < scan.size(); i++) {
+		for (unsigned int j = i + 1; j < scan.size(); j++) {
+			if (scan[i].abs(scan[j]) > max_distance) {
+				max_distance = scan[i].abs(scan[j]);
+				p1 = scan[i];
+				p2 = scan[j];
+			}
+		}
+	}
+
+
+	glutMainLoop();
 	return 0;
 }
